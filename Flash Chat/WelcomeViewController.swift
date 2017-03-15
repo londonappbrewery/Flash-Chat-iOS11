7b25b633f902f64bc38b6ef7de7d180c77ebb9e3
angelabauer <angela_yu@hotmail.co.uk>
2017-03-15 10:48:14 +0000

    updated TODOs

--- Flash Chat/WelcomeViewController.swift
+++ Flash Chat/WelcomeViewController.swift
@@ -14,9 +14,6 @@ class WelcomeViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //TODO: Check to see if there is a user logged in already, if so send them straight through to the ChatViewController.
-    
 
     
     }
