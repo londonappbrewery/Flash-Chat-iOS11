a695ab23892e5f19fb615c1da02edbc920be862b
Bill Glover <bill@billglover.co.uk>
2017-12-17 16:39:29 +0000

    Publish data to the Firebase database

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,8 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        //TODO: Initialise and Configure your Firebase here:
+        // Initialise and Configure your Firebase here:
+        FirebaseApp.configure()
         
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("We've got data! ")
         
         return true
     }
