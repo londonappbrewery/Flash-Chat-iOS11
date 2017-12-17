ece04e8e11995236fd844a8019ea558bbcec36f2
Bill Glover <bill@billglover.co.uk>
2017-12-17 18:06:16 +0000

    Implement log in and log out functionality

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class ChatViewController: UIViewController {
     
@@ -106,9 +106,15 @@ class ChatViewController: UIViewController {
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
-        //TODO: Log out the user and send them back to WelcomeViewController
+        //Log out the user and send them back to WelcomeViewController
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        }
         
-        
+        catch {
+            print("error, there was a problem signing out.")
+        }
     }
     
 
--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,6 +6,7 @@
 
 
 import UIKit
+import Firebase
 
 
 class LogInViewController: UIViewController {
@@ -25,11 +26,15 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
-
-        
-        //TODO: Log in the user
-        
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            if error != nil {
+                print("\(error!.localizedDescription)")
+                return
+            }
+            
+            print("Log in successsful!")
+            self.performSegue(withIdentifier: "goToChat", sender: self)
+        }
     }
     
 
