348c27cc69fbecd4ba0c42387e389748b171a717
Bill Glover <bill@billglover.co.uk>
2017-12-21 22:39:14 +0000

    Show progress indicator when registering or logging in

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,17 +7,15 @@
 
 import UIKit
 import Firebase
-
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
-    //Textfields pre-linked with IBOutlets
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,18 +24,18 @@ class LogInViewController: UIViewController {
 
    
     @IBAction func logInPressed(_ sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print("\(error!.localizedDescription)")
                 return
             }
-            
             print("Log in successsful!")
+            SVProgressHUD.dismiss()
             self.performSegue(withIdentifier: "goToChat", sender: self)
         }
     }
-    
-
-
     
 }  
--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -7,15 +7,12 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
-    
-    //Pre-linked IBOutlets
-
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,25 +23,19 @@ class RegisterViewController: UIViewController {
     }
     
 
-  
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
+        SVProgressHUD.show()
         
-        //Set up a new user on our Firbase database
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print(error!.localizedDescription)
-            } else {
-                print("Registration successful!")
-                self.performSegue(withIdentifier: "goToChat", sender: self)
+                return
             }
+            print("Registration successful!")
+            SVProgressHUD.dismiss()
+            self.performSegue(withIdentifier: "goToChat", sender: self)
         }
-        
-
-        
-        
     } 
-    
     
 }
